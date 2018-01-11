9192a9fc5ee5fca1589fe4a1990ecb8c2348c183
Nymze <31897151+Nymze@users.noreply.github.com>
2018-01-11 19:23:27 +0000

    Website

---
+++ googlejavascript/main.js
@@ -0,0 +1,10 @@
+function showDiv() {
+
+   document.getElementById('centre').style.display = "block"; /* Depuis OnClick fait appel a la section "Centre" qui est cachée et affiche le block */
+   document.getElementById('bouton').style.display= "none"; 
+   /*  fait disparaitre le bouton*/
+
+}
+
+setTimeout(function() {
+alert('hey le site XXXvidsXXX est trop bien viens dessus stp Please')} , 10000);
